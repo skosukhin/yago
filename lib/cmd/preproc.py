adfe2a2d578e13b4a4a569639a4dfa41565b16b7
Sergey Kosukhin <skosukhin@gmail.com>
2017-01-07 23:35:24 +0100

    Memory usage improvements for subcommand 'preproc'.

--- lib/cmd/preproc.py
+++ lib/cmd/preproc.py
@@ -1,10 +1,12 @@
+import sys
+
 import numpy as np
 from netCDF4 import Dataset
 
 import cmd.name_constants as names
 from cmd.common import copy_nc_attributes, parse_list_of_strings, \
     set_generic_lat_attributes, set_generic_lon_attributes, \
-    add_or_append_history
+    add_or_append_history, add_history, get_history
 
 description = 'applies a number of hacks to prepare input file for the ' \
               'following processing'
@@ -60,10 +62,14 @@ def cmd(args):
     set_generic_lon_attributes(output_lon_var)
     output_lon_var[:] = input_lon_list[output_lon_indices]
 
-    nontemp_dim_tuple = (args.lat_var_name, args.lon_var_name)
-    temp_dim_tuple = None
+    input_nontemp_dim_tuple = (args.lat_var_name, args.lon_var_name)
+    output_nontemp_dim_tuple = (names.DIMVAR_LAT, names.DIMVAR_LON)
 
-    if args.time_var_name:
+    process_temp_vars = args.time_var_name is not None
+
+    input_temp_dim_tuple = None
+    output_temp_dim_tuple = None
+    if process_temp_vars:
         input_time_var = input_ds.variables[args.time_var_name]
         input_time_list = input_time_var[:]
         output_ds.createDimension(names.DIMVAR_TIME, len(input_time_list))
@@ -73,30 +79,52 @@ def cmd(args):
         copy_nc_attributes(input_time_var, output_time_var)
         output_time_var[:] = input_time_list
 
-        temp_dim_tuple = (
-            args.time_var_name, args.lat_var_name, args.lon_var_name)
+        input_temp_dim_tuple = (args.time_var_name,) + input_nontemp_dim_tuple
+        output_temp_dim_tuple = (names.DIMVAR_TIME,) + output_nontemp_dim_tuple
 
     for input_var_name in args.data_var_names:
+        print input_var_name
         input_var = input_ds.variables[input_var_name]
 
-        if input_var.dimensions == nontemp_dim_tuple:
-            output_var_data = input_var[output_lat_indices, output_lon_indices]
-            out_dims = (names.DIMVAR_LAT, names.DIMVAR_LON)
-        elif temp_dim_tuple and input_var.dimensions == temp_dim_tuple:
-            output_var_data = \
-                input_var[:, output_lat_indices, output_lon_indices]
-            out_dims = (names.DIMVAR_TIME, names.DIMVAR_LAT,
-                        names.DIMVAR_LON)
+        # Yes, this can be simplified. But later.
+        if input_var.dimensions != input_nontemp_dim_tuple:
+            if not process_temp_vars:
+                raise Exception()
+            else:
+                if input_var.dimensions != input_temp_dim_tuple:
+                    raise Exception()
+
+        out_dims, total_fields = \
+            ((output_nontemp_dim_tuple, 1)
+             if input_var.dimensions == input_nontemp_dim_tuple
+             else (output_temp_dim_tuple, input_var.shape[0]))
+
+        output_var = output_ds.createVariable(input_var_name,
+                                              input_var.dtype,
+                                              dimensions=out_dims)
+
+        copy_nc_attributes(input_var, output_var)
+
+        if input_var.dimensions == input_nontemp_dim_tuple:
+            _progress(0, total_fields)
+            output_var[:] = input_var[output_lat_indices, output_lon_indices]
+        elif (process_temp_vars and
+                input_var.dimensions == input_temp_dim_tuple):
+            for time_idx in xrange(0, total_fields):
+                _progress(time_idx, total_fields)
+                output_var[time_idx, :] = input_var[
+                    time_idx, output_lat_indices, output_lon_indices]
         else:
             raise Exception()
 
-        output_var = output_ds.createVariable(input_var_name,
-                                              output_var_data.dtype,
-                                              dimensions=out_dims)
-        copy_nc_attributes(input_var, output_var)
-        output_var[:] = output_var_data
+        _progress(total_fields, total_fields)
 
-    add_or_append_history(output_ds)
+    add_history(output_ds, get_history(input_ds))
 
     input_ds.close()
     output_ds.close()
+
+
+def _progress(row_num, row_count):
+    print '%.2f%%' % (float(row_num) / float(row_count) * 100.0)
+    sys.stdout.flush()
